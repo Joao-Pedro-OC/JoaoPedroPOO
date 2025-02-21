c1fdffea7138d585d292bfb61e316bff078d3a41
Joao-Pedro-OC <joaopedrocavalcante630@gmail.com>
2025-02-20 22:01:22 -0300

    Questão 1

---
+++ do-while/src/pkgdo/pkgwhile/ques1.java
@@ -0,0 +1,20 @@
+
+// 1 - Considere uma população inicial de 1000 habitantes e uma taxa de crescimento de 2% ao ano. Utilize do-while para calcular quantos anos serão necessários para a população ultrapassar 5000 habitantes.
+
+package pkgdo.pkgwhile;
+
+public class ques1 {
+    
+    public static void main(String[] args) {
+        
+        int população = 1000, anos = 0;
+        
+        do {
+            população += população * 0.02;
+            ++anos;
+        } while (população < 5000);
+        
+        System.out.println("Para a populacao vai ter "+população+" habitantes depois de " + anos + " anos.");
+    }
+    
+}
